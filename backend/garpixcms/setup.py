6fc8144f76008bae787a905aa2016cbf5cc5e457
crusat <crusat@yandex.ru>
2021-08-12 19:22:57 +0300

    Upgrade to 1.15.0

--- backend/garpixcms/setup.py
+++ backend/garpixcms/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='garpixcms',
-    version='1.14.0',
+    version='1.15.0',
     description='',
     long_description=long_description,
     url='https://github.com/garpixcms/garpixcms',
@@ -35,7 +35,7 @@ setup(
         'garpix_menu == 1.0.2',
         'garpix_qa == 1.0.6',
         'garpix_auth == 1.1.1',
-        'garpix_notify == 3.0.3',
+        'garpix_notify == 4.0.0',
         'garpix_package == 2.0.0',
         'psycopg2-binary >= 2.8.6',
         'uwsgi >= 2.0.19.1',
