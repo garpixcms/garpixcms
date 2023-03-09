79fe7bac2a454fceb1a543d72b706aa46331b775
griviala <78097561+griviala@users.noreply.github.com>
2023-03-09 06:46:16 +0300

    4.0.0-rc1

--- backend/garpixcms/setup.py
+++ backend/garpixcms/setup.py
@@ -8,7 +8,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='garpixcms',
-    version='4.1.0-rc1',
+    version='4.0.0-rc1',
     description='',
     long_description=long_description,
     url='https://github.com/garpixcms/garpixcms',
